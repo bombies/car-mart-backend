7e94fbd0cb584e6c55ee9df87e7a590aa8093e52
Michael Leighton <michael138leighton@gmail.com>
2023-01-21 20:46:24 -0500

    added cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import helmet from "helmet";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin : ['*', 'http://localhost:4200'],
+    methods: ['POST', 'PUT', 'DELETE', 'GET']
+  });
   app.use(helmet())
   await app.listen(3000);
 }
