957b1f22cb05b1ddcd6aef819999fcdc553fcd88
bombies <xjamesdzn@gmail.com>
2023-01-22 10:12:08 -0500

    Fix redis integratrion with environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,7 +8,7 @@ import { APP_GUARD } from "@nestjs/core";
 import { PermissionGuard } from "./utils/permissions/permission.guard";
 import { JwtAuthGuard } from "./auth/guards/jwt-auth.guard";
 import { AuthController } from "./auth/route/auth.controller";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { AuthService } from "./auth/route/auth.service";
 import { JwtService } from "@nestjs/jwt";
 import { UsersModule } from "./users/users.module";
@@ -24,17 +24,23 @@ import { Logger } from "@nestjs/common/services";
 
 @Module({
     imports: [
-        RedisModule.forRoot({
-            config: {
-                host: "localhost",
-                port: 6379,
-                password: process.env.REDIS_PASSWORD,
-                onClientCreated(client) {
-                    client.on('ready', () => {
-                        Logger.log("Redis is ready to be used!")
-                    })
+        RedisModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory:async (configService: ConfigService) => {
+                return {
+                    config: {
+                        host: configService.get("REDIS_HOST"),
+                        port: configService.get("REDIS_PORT"),
+                        password: configService.get("REDIS_PASSWORD"),
+                        onClientCreated(client) {
+                            client.on('ready', () => {
+                                Logger.log("Redis is ready to be used!")
+                            })
+                        }
+                    },
                 }
-            },
+            }
         }),
         ConfigModule.forRoot({
             isGlobal: true,
